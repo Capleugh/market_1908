321930bf5ac45103680c3601982e27d6376085cb
Carleigh Crockett <capleugh@gmail.com>
2019-09-11 14:38:29 -0600

    add vendors_that_sell method and test

--- lib/market.rb
+++ lib/market.rb
@@ -14,7 +14,11 @@ class Market
     @vendors.map do |vendor|
       vendor.name
     end
-
   end
 
+  def vendors_that_sell(item)
+    @vendors.find_all do |vendor|
+      vendor.check_stock(item) > 0
+    end
+  end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -33,4 +33,19 @@ class MarketTest < Minitest::Test
     @market.add_vendor(@vendor_3)
     assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
   end
+
+  def test_vendors_can_sell_items
+    @market.add_vendor(@vendor_1)
+    @vendor_1.stock("Peaches", 35)
+    @vendor_1.stock("Tomatoes", 7)
+    @market.add_vendor(@vendor_3)
+    @vendor_3.stock("Peaches", 65)
+    @market.add_vendor(@vendor_2)
+    @vendor_2.stock("Banana Nice Cream", 50)
+    @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    assert_equal [@vendor_1, @vendor_3], @market.vendors_that_sell("Peaches")
+    assert_equal [@vendor_2], @market.vendors_that_sell("Banana Nice Cream")
+  end
+
+
 end
