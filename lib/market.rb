6045b13e85da8e2e8da1302f7d70781df8799844
Carleigh Crockett <capleugh@gmail.com>
2019-09-11 14:15:19 -0600

    Adds initialize and add_vendor methods

---
+++ lib/market.rb
@@ -0,0 +1,17 @@
+class Market
+  attr_reader :name, :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  # def vendor_names
+  #   @vendor
+  # end
+
+end
